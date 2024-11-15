3df485c8d92cce6632fc1b600524db7948c20722
monikapatidar27 <monikapatidar33440@gmail.com>
2024-11-15 19:42:48 +0530

    changes in mongoose

--- database/dbConnect.js
+++ database/dbConnect.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose");
 
 const dbConnect = () => {
     mongoose
-        .connect(process.env.DB_URL,{
+        .connect(process.env.DB_URL||"mongodb+srv://mpatidar:GTAlQSULi17j8jEh@cluster0.n2ptq.mongodb.net/userDetails?retryWrites=true&w=majority"
+,{
             useNewUrlParser:true,
             useUnifiedTopology: true,
         })
